4691400cfd6138cd9101fa472f5349a392f6938c
Gyrth <gyrthmcmulin@gmail.com>
2022-02-04 16:50:45 +0100

    New initial commit

---
+++ blacktrace_demo_1/Blacktrace.pck
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:660f68815b77ae8b74283060b933ec5ee3e69140a3a04bffe3beec0c195aa441
+size 251712832
---
+++ javascripts/add_common_items.js
@@ -0,0 +1,68 @@
+addHeader();
+addNav();
+addFooter();
+
+$( document ).ready(function(){
+	$(".button-collapse").sideNav();
+})
+
+function addHeader(){
+	document.querySelector ('header').innerHTML =
+	'<link rel="shortcut icon" type="image/jpg" href="favicon.ico"/>'+
+	'<h1><img src="images/avatar.png" alt="" width="45"></image> Gyrth McMulin</h1>'+
+	'<h3>Programmer</h3>'+
+	'<hr></hr>';
+}
+
+function addNav() {
+	document.getElementById("navigator").innerHTML =
+	'<nav>'+
+		'<div class="nav-wrapper">'+
+		  '<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>'+
+			'<ul class="left hide-on-med-and-down">'+
+				'<li><a href="index.html">Main Page</a></li>'+
+				'<li><a href="overgrowth.html">Overgrowth</a></li>'+
+				'<li><a href="school_projects.html">School Projects</a></li>'+
+				'<li><a href="moonwards.html">Moonwards</a></li>'+
+				'<li><a href="blacktrace.html">Blacktrace</a></li>'+
+			'</ul>'+
+			'<ul class="right hide-on-med-and-down">'+
+				'<li><a href="https://www.youtube.com/channel/UCS_9m_pTohKCh2kyKfk6xXw"><div class="right soc-med"><img src="images/youtube.png"></div></a></li>'+
+				'<li><a href="https://twitter.com/GyrthMcMulin"><div class="right soc-med"><img src="images/twitter.png"></div></a></li>'+
+				'<li><a href="https://www.linkedin.com/in/hendrik-wiersma/"><div class="right soc-med"><img src="images/linkedin.png"></div></a></li>'+
+			'</ul>'+
+			'<ul class="side-nav" id="mobile-demo">'+
+				'<li><a href="index.html">Main Page</a></li>'+
+				'<li><a href="overgrowth.html">Overgrowth</a></li>'+
+				'<li><a href="school_projects.html">School Projects</a></li>'+
+				'<li><a href="moonwards.html">Moonwards</a></li>'+
+				'<li><a href="blacktrace.html">Blacktrace</a></li>'+
+				'<li><a href="https://www.youtube.com/channel/UCS_9m_pTohKCh2kyKfk6xXw">YouTube<div class="right soc-med"><img src="images/youtube.png"></div></a></li>'+
+				'<li><a href="https://twitter.com/GyrthMcMulin">Twitter<div class="right soc-med"><img src="images/twitter.png"></div></a></li>'+
+				'<li><a href="https://www.linkedin.com/in/hendrik-wiersma/">LinkedIn<div class="right soc-med"><img src="images/linkedin.png"></div></a></li>'+
+			'</ul>'+
+		'<div>'+
+	'</nav>';
+}
+
+function addFooter(){
+	document.querySelector ('footer').innerHTML =
+	'<p><small>Hosted on GitHub Pages<br>'+
+	'Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>';
+}
+
+var myvid = document.getElementById('playlist');
+var player = $('#playlist')
+var videos = player.data('playlist');
+var activeVideo = 0;
+
+if(myvid != null){
+	myvid.addEventListener('ended', function(e) {
+		// update the new active video index
+		activeVideo = (++activeVideo) % videos.length;
+
+		// update the video source and play
+		myvid.src = videos[activeVideo];
+		myvid.play();
+	});
+};
